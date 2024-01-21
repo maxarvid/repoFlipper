4b1ed7b767b0a138c6747fb095a6476bd45e0abb
maxarvid <max@klimato.se>
2024-01-21 15:43:34 +0100

    ➕ Adds octokit with simple repo index call

--- src/components/FlipperForm.vue
+++ src/components/FlipperForm.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+// TODO: check out octokit/core probably enough
+import { Octokit } from 'octokit';
+
+const handleSubmit = async () => {
+    const octokit = new Octokit({ auth: token.value });
+    const { data } = await octokit.rest.repos.listForUser({ username: username.value })
+    console.log(data);
+}
 
 const username = ref('')
 const token = ref('')
-
-
 </script>
 
 <template>
-    <form>
+    <form @submit.prevent="handleSubmit">
         <label for="username">Username</label>
         <input type="text" id="username" name="username" v-model="username" />
         <label for="token">Token</label>
